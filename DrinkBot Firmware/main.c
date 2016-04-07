42914132926b038d003a6a7aed75693a2103ebe1
tedfmyers <tedfmyers@gmail.com>
2016-04-07 19:26:26 -0400

    Updated interrupt vector stuff, other minor updates
    Cleaned up the defines a bit
    
    Added some more comments
    
    Thought about vector logic a little

--- DrinkBot Firmware/main.c
+++ DrinkBot Firmware/main.c
@@ -34,44 +34,65 @@
 #include <util/delay.h>
 #include <stdbool.h> // for boolean values
 
-#define RELAY_PORT PORTB    // port relay connected to
-#define RELAY_DDR DDRB      // port data direction register
-#define RELAY_PIN PB1       // relay: controls drink flow (on/off) - can be P1?
+// Relay: controls drink pouring
+#define RELAY_PORT  PORTB   // port relay connected to
+#define RELAY_DDR   DDRB    // port data direction register
+#define RELAY_PIN   PB1     // relay: controls drink flow (on/off) - can be P1?
 
 // Switches: Red and Green, both with an integrated LED
-#define SW_PORT 0       // port switches are connected to
-#define SW_DDR 0        // port data direction register
-#define SW_GREEN 0      // Switch 1 (green?)
-#define SW_RED 0        // Switch 2 (red?)
-#define SW_RED_LED 0    // LED for switch 1
-#define SW_GREEN_LED 0  // LED for switch 2
+#define SW_PORT         0   // port switches are connected to
+#define SW_DDR          0   // port data direction register
+#define SW_PIN          0   // pin for button port
+#define SW_GREEN        0   // Switch 1
+#define SW_RED          0   // Switch 2
+#define SW_RED_LED      0   // LED for switch 1
+#define SW_GREEN_LED    0   // LED for switch 2
+#define SW_INT_VECT     0   // Interrupt vector that corresponds to SW_port pins    (Ex: PCINT1_vect)
+#define SW_INT_FLAG     0   // Interrupt flag to set                                (Ex: PCIE1)
 
 // Analog Potentiometer
 #define POT_PORT 0
-#define POT_PIN 0
+#define POT_PIN  0
 
-
-
+// pouring
 #define POUR_ON true //boolean values for input to pourDrink function
 #define POUR_OFF false
 
-volatile bool pour = false;
+volatile bool pour_drink = false;
 volatile int pourTime; // time pump is on (in milliseconds)
 
 void initIO(void)
 {
     // Set Pins as Inputs or Outputs
-    RELAY_DDR |= (1<<RELAY_PIN);                    // Set relay pin (PB1) as output (check that it is correct pin)
-    SW_DDR &= ~( (1<<SW_GREEN) || (1<<SW_RED) );    // initialize switch 1 and 2 as inputs
-    SW_DDR |= ( (1<<SW_GREEN_LED) || (1<<SW_RED_LED) ); // initialize leds in buttons as outputs
+    RELAY_DDR   |= (1<<RELAY_PIN);                    // Set relay pin (PB1) as output (check that it is correct pin)
+    SW_DDR      &= ~( (1<<SW_GREEN) | (1<<SW_RED) );    // initialize switch 1 and 2 as inputs
+    SW_DDR      |= ( (1<<SW_GREEN_LED) | (1<<SW_RED_LED) ); // initialize leds in buttons as outputs
 
     // Enable internal pull-up resistors on switches
-    SW_PORT |= _BV(SW_RED) || _BV(SW_GREEN);
+    SW_PORT |= _BV(SW_RED) | _BV(SW_GREEN);
     
     // Make sure relay is off, just in case
     RELAY_PORT &= ~_BV(RELAY_PIN);
     
 }
+
+/*
+ 
+ ISR(PCINT1_vect){
+ //Turn on all of port D
+ PORTD |= (1 << PD5);
+ }
+ 
+ void initIO(void) {
+ //Define PC3 as input with pullup-resistor
+ DDRC &= ~(1 << button);
+ PORTC |= (1 << button);
+ 
+ //Enable interrupt for PCINT11
+ PCICR |= _BV(PCIE1);
+ PCMSK1 |= (_BV(button_interrupt));
+ 
+ */
 
 //pours for pourTime milliseconds while (volatile) pour is true
 void pourDrink(void) // uses volatile global variables to pour drink
@@ -82,7 +103,7 @@ void pourDrink(void) // uses volatile global variables to pour drink
     int pourCounter = pourTime;
     
     // pour drink, check every ms
-    while ( pour && pourCounter )
+    while ( pour_drink && pourCounter )
     {
         RELAY_PORT |= _BV(RELAY_PIN);
         _delay_ms(1);
@@ -93,13 +114,33 @@ void pourDrink(void) // uses volatile global variables to pour drink
     
 }
 
-ISR() // put in vector
+ISR(SW_INT_VECT) // put in vector
 {
+    switch ( SW_PIN )
+    {
+        case 1<<SW_GREEN:                   // green button pressed
+            
+        case 1<<SW_RED:                     // red switch pressed
+            
+        case (1<<SW_GREEN) | (1<<SW_RED):   // both switched pressed
+            // Only green switch pressed
+    }
+    
+    
     // ISR: if either "pour" button has been pressed
     // light correct button while being pressed
     // If green button, pour while it is pressed
     // If red button, set pourTime to equivalent POT_PIN value
     // and set bool pour = true
+    
+    // GR
+    // 00           All off
+    // 01           Pour based on time
+    // 10           Pour while pressed
+    // 11           Pour while pressed, then based on time
+    // 01 -> 11     Initially: pour based on time; then stop
+    // 10 -> 11     Initially: pour while pressed; then stop
+    // 11 -> 10/01  keep pouring
 }
 
 
@@ -107,7 +148,7 @@ int main(void)
 {
 	initIO();
     
-    if (pour)
+    if (pour_drink)
         pourDrink();
     
     
